a5f7e515ffaa8b81e04ef0c9c9eee26da0321042
Wilton Souza <willsouzafilho@gmail.com>
2022-02-02 11:12:30 -0300

    Adding the user delete route

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -1,7 +1,9 @@
 defmodule Rockelivery do
   alias Rockelivery.Users.Create, as: UsersCreate
+  alias Rockelivery.Users.Delete, as: UsersDelete
   alias Rockelivery.Users.Get, as: UsersGet
 
   defdelegate create_user(params), to: UsersCreate, as: :call
+  defdelegate delete_user(id), to: UsersDelete, as: :call
   defdelegate get_user_by_id(id), to: UsersGet, as: :by_id
 end
