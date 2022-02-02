dc63ae3da34d8b3471380e777b3ed4279468f87e
Wilton Souza <willsouzafilho@gmail.com>
2022-02-02 11:41:09 -0300

    Refactoring error handler

--- lib/rockelivery/users/get.ex
+++ lib/rockelivery/users/get.ex
@@ -1,6 +1,6 @@
 defmodule Rockelivery.Users.Get do
   alias Ecto.UUID
-  alias Rockelivery.{Repo, User}
+  alias Rockelivery.{Error, Repo, User}
 
   # EXEMPLO USANDO O WITH
   # def by_id(id) do
@@ -15,14 +15,14 @@ defmodule Rockelivery.Users.Get do
 
   def by_id(id) do
     case UUID.cast(id) do
-      :error -> {:error, %{status: :bad_request, result: "Invalid id format!"}}
+      :error -> {:error, Error.build_id_format_error()}
       {:ok, uuid} -> get(uuid)
     end
   end
 
   defp get(id) do
     case Repo.get(User, id) do
-      nil -> {:error, %{status: :not_found, result: "User not found"}}
+      nil -> {:error, Error.build_user_not_found_error()}
       user -> {:ok, user}
     end
   end
