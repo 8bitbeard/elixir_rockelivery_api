3ce8c774b2b075513ce4038acd3314077e341650
Wilton Souza <willsouzafilho@gmail.com>
2022-02-03 05:42:07 -0300

    Adding the user update route

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -2,8 +2,10 @@ defmodule Rockelivery do
   alias Rockelivery.Users.Create, as: UsersCreate
   alias Rockelivery.Users.Delete, as: UsersDelete
   alias Rockelivery.Users.Get, as: UsersGet
+  alias Rockelivery.Users.Update, as: UsersUpdate
 
   defdelegate create_user(params), to: UsersCreate, as: :call
   defdelegate delete_user(id), to: UsersDelete, as: :call
   defdelegate get_user_by_id(id), to: UsersGet, as: :by_id
+  defdelegate update_user(params), to: UsersUpdate, as: :call
 end
