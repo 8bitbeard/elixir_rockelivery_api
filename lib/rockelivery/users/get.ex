2cd374565e4a829cf69b17ee989bb10f024ece7c
Wilton Souza <willsouzafilho@gmail.com>
2022-02-03 06:03:58 -0300

    Updating delete get and update to use the new UUID Plug

--- lib/rockelivery/users/get.ex
+++ lib/rockelivery/users/get.ex
@@ -1,5 +1,4 @@
 defmodule Rockelivery.Users.Get do
-  alias Ecto.UUID
   alias Rockelivery.{Error, Repo, User}
 
   # EXEMPLO USANDO O WITH
@@ -14,13 +13,6 @@ defmodule Rockelivery.Users.Get do
   # end
 
   def by_id(id) do
-    case UUID.cast(id) do
-      :error -> {:error, Error.build_id_format_error()}
-      {:ok, uuid} -> get(uuid)
-    end
-  end
-
-  defp get(id) do
     case Repo.get(User, id) do
       nil -> {:error, Error.build_user_not_found_error()}
       user -> {:ok, user}
