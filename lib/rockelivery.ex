4f9fc1023f07c7914f63caac895576f3a0b2f4ee
Wilton Souza <willsouzafilho@gmail.com>
2022-02-08 06:29:07 -0300

    Creating item migrations, schema, controller, view and route

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -1,4 +1,6 @@
 defmodule Rockelivery do
+  alias Rockelivery.Items.Create, as: ItemsCreate
+
   alias Rockelivery.Users.Create, as: UsersCreate
   alias Rockelivery.Users.Delete, as: UsersDelete
   alias Rockelivery.Users.Get, as: UsersGet
@@ -8,4 +10,6 @@ defmodule Rockelivery do
   defdelegate delete_user(id), to: UsersDelete, as: :call
   defdelegate get_user_by_id(id), to: UsersGet, as: :by_id
   defdelegate update_user(params), to: UsersUpdate, as: :call
+
+  defdelegate create_item(params), to: ItemsCreate, as: :call
 end
